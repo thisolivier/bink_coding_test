76d0cbd2e7378a0abd000b10fc514a2409aae025
Olivier Butler <olivier.butler@gmail.com>
2019-11-27 12:43:48 +0000

    Fixed stubs

--- phonemaster/phonemast_leases.py
+++ phonemaster/phonemast_leases.py
@@ -3,21 +3,23 @@ def current_rent_ascending_order():
     1) Produce a list sorted by “Current Rent” in ascending order
     2) Obtain the first 5 items from the resultant list and output to the console
     """
-    return
+    pass
 
 def lease_length_25_years():
     """ Requirements
     1) Output the list to the console, including all data fields
     2) Output the total rent for all items in this list to the console
     """
-    return 
+    pass 
 
 def masts_count_per_tenant():
     """ Requirements
     1) Output the dictionary to the console in a readable form
     """
+    pass
 
 def lease_start_date_1june1999_to_31aug2007():
     """ Requirements
     1) Output the data to the console with dates formatted as DD/MM/YYYY
     """
+    pass
