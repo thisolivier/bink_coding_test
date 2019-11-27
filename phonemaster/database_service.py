d84ab3717b05f49394bf44b6959cd6aa41dbd4dd
Olivier Butler <olivier.butler@gmail.com>
2019-11-27 21:44:48 +0000

    Two down

--- phonemaster/database_service.py
+++ phonemaster/database_service.py
@@ -1,7 +1,11 @@
 import csv
 import os.path
+from datetime import datetime
 
 _csv_data = open(os.path.join(os.path.dirname(__file__), 'database.csv'))
+
+def _date_formatter(date_string):
+    return datetime.strptime(date_string, '%d %b %Y')
 
 class PhoneMastHelper():
     @staticmethod
@@ -22,25 +26,52 @@ class PhoneMastHelper():
 
     @staticmethod
     def lease_start_date(csv_row):
-        return csv_row[7]
+        return _date_formatter(csv_row[7])
 
     @staticmethod
     def lease_end_date(csv_row):
-        return csv_row[8]
+        return _date_formatter(csv_row[8])
     
     @staticmethod
     def lease_years(csv_row):
-        print("years", csv_row[9])
         return int(csv_row[9])
 
     @staticmethod
     def rent(csv_row):
-        return csv_row[10]
+        print(csv_row)
+        return float(csv_row[10])
+
+    @staticmethod
+    def validate_row(row):
+        try:
+            if (PhoneMastHelper.property_name(row) != "",
+                PhoneMastHelper.address(row)[0] != "",
+                PhoneMastHelper.unit_name(row) != "",
+                PhoneMastHelper.tenant_name(row) != "",
+                PhoneMastHelper.lease_start_date(row) is datetime,
+                PhoneMastHelper.lease_end_date(row) is datetime,
+                PhoneMastHelper.lease_years(row) != 0,
+                PhoneMastHelper.rent(row) != 0):
+                    return True
+            else:
+                return False
+        except:
+            return False
+
 
 def get_masts_iterable():
-    _csv_data.seek(1)
+    _csv_data.seek(0)
     reader = csv.reader(_csv_data)
-    _csv_data.seek(1)
+    next(reader)
     return reader
 
+"""
+Check we have real data to work with
+"""
+if _csv_data is None:
+    raise("Could not open CSV file")
+for row in get_masts_iterable():
+    if PhoneMastHelper.validate_row(row) == False:
+        raise("One or more rows of the CSV file contains bad data")
 
+print("Opened and validated CSV data, ready to go")
--- phonemaster/phonemast_leases.py
+++ phonemaster/phonemast_leases.py
@@ -1,12 +1,14 @@
 from database_service import get_masts_iterable, PhoneMastHelper
 from console_print_views import rent_view
 
+_get_masts_iterable = get_masts_iterable
+
 def current_rent_ascending_order():
     """ Requirements
     1) Produce a list sorted by “Current Rent” in ascending order
     2) Obtain the first 5 items from the resultant list and output to the console
     """
-    masts_sorted_by_rent = sorted(get_masts_iterable(), key=PhoneMastHelper.rent)[:5]
+    masts_sorted_by_rent = sorted(_get_masts_iterable(), key=PhoneMastHelper.rent)[:5]
     description = "The first five items in list of masts sorted by rent"
     rent_view(description, masts_sorted_by_rent)
     return masts_sorted_by_rent
@@ -16,9 +18,12 @@ def lease_length_25_years():
     1) Output the list to the console, including all data fields
     2) Output the total rent for all items in this list to the console
     """
-    masts_with_25_year_lease = [mast for mast in get_masts_iterable() if PhoneMastHelper.lease_years(mast)==25]
-    print(masts_with_25_year_lease)
-    return masts_with_25_year_lease
+    masts_with_25_year_lease = [mast for mast in _get_masts_iterable() if PhoneMastHelper.lease_years(mast) == 25]
+    total_rents = 0.0
+    for mast in masts_with_25_year_lease:
+        total_rents += PhoneMastHelper.rent(mast)
+    print(total_rents)
+    return (masts_with_25_year_lease, total_rents)
 
 def masts_count_per_tenant():
     """ Requirements
