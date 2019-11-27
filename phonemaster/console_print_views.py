3333e668216c3cdcb81daf35f0096fa925c35a5b
Olivier Butler <olivier.butler@gmail.com>
2019-11-27 22:18:51 +0000

    Logic all written, just need to finish views and write real tests

--- phonemaster/console_print_views.py
+++ phonemaster/console_print_views.py
@@ -4,15 +4,32 @@ large_divider = "---------------------------------"
 small_divider = "--------"
 introduction = "Phonemaster says:"
 
-def rent_view(description, iterableMasts):
-    def pretty_print_masts(someMast):
-        tuple_rent_name_tenant = (PhoneMastHelper.rent(someMast), PhoneMastHelper.property_name(someMast), PhoneMastHelper.tenant_name(someMast))
-        print("[%s] %s || %s" %tuple_rent_name_tenant)
-
+def _print_header(description):
     print(large_divider)
     print(introduction)
     print("-", description)
+
+def view_rent_ascending(description, iterable_masts):
+    def pretty_print_masts(someMast):
+        tuple_rent_name_tenant = (PhoneMastHelper.rent(someMast), PhoneMastHelper.property_name(someMast), PhoneMastHelper.tenant_name(someMast))
+        print("[%s] %s || %s" %tuple_rent_name_tenant)
+    _print_header(description)
     print("- Format will be: [Rent] - Location Name || Tenant")
     print(small_divider)
-    [pretty_print_masts(mast) for mast in iterableMasts]
+    [pretty_print_masts(mast) for mast in iterable_masts]
+    print(large_divider)
+
+def view_lease_lengths_with_total(description, iterable_masts, total):
+    _print_header(description)
+    
+    print(large_divider)
+
+def view_mast_count_dict(description, tenant_name_count_dict):
+    _print_header(description)
+
+    print(large_divider)
+
+def view_masts_in_date_range(description, iterable_masts):
+    _print_header(description)
+
     print(large_divider)
--- phonemaster/phonemast_leases.py
+++ phonemaster/phonemast_leases.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from database_service import get_masts_iterable, PhoneMastHelper
-from console_print_views import rent_view
+from console_print_views import view_rent_ascending, view_masts_in_date_range, view_mast_count_dict, view_lease_lengths_with_total
 
 _get_masts_iterable = get_masts_iterable
 
@@ -9,8 +10,10 @@ def current_rent_ascending_order():
     2) Obtain the first 5 items from the resultant list and output to the console
     """
     masts_sorted_by_rent = sorted(_get_masts_iterable(), key=PhoneMastHelper.rent)[:5]
+
     description = "The first five items in list of masts sorted by rent"
-    rent_view(description, masts_sorted_by_rent)
+    view_rent_ascending(description, masts_sorted_by_rent)
+
     return masts_sorted_by_rent
 
 def lease_length_25_years():
@@ -22,28 +25,49 @@ def lease_length_25_years():
     total_rent = 0.0
     for mast in masts_with_25_year_lease:
         total_rent += PhoneMastHelper.rent(mast)
-    print(total_rent)
+
+    description = "All phone masts with 25 year leases, and their total rent"
+    view_lease_lengths_with_total(description, masts_with_25_year_lease, total_rent)
+
     return (masts_with_25_year_lease, total_rent)
 
 def masts_count_per_tenant():
     """ Requirements
     1) Output the dictionary to the console in a readable form
     """
-    tenants = {}
+    tenants_dict = {}
     for mast in _get_masts_iterable():
         tenant = PhoneMastHelper.tenant_name(mast)
-        if tenant in tenants:
-            tenants[tenant] += 1
+        if tenant in tenants_dict:
+            tenants_dict[tenant] += 1
         else:
-            tenants[tenant] = 1
-    print(tenants)
-    return tenants
+            tenants_dict[tenant] = 1
+    
+    description = "Tenants names and the number of masts they lease"
+    view_mast_count_dict(description, tenants_dict)
+
+    return tenants_dict
 
 def lease_start_date_1june1999_to_31aug2007():
     """ Requirements
     1) Output the data to the console with dates formatted as DD/MM/YYYY
     """
-    pass
+    range_start = datetime(1999, 6, 1)
+    range_end = datetime(2007, 7, 31)
+    def filter_only_in_range(mast):
+        lease_start = PhoneMastHelper.lease_start_date(mast)
+        return range_start < lease_start < range_end
 
-if __name__ == '__main__':
+    masts_in_range = list(filter(filter_only_in_range, _get_masts_iterable()))
+    
+    description = "Masts with a lease starting between 01/06/1999 and 31/07/2007"
+    view_masts_in_date_range(description, masts_in_range)
+
+    return masts_in_range
+
+def run_all_queries():
+    current_rent_ascending_order()
+    lease_length_25_years()
+    masts_count_per_tenant()
     lease_start_date_1june1999_to_31aug2007()
+    print("Thanks for executing, have a nice day!")
