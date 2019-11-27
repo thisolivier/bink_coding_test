17c466384d75342363ae7a80d320c73329547527
Olivier Butler <olivier.butler@gmail.com>
2019-11-27 12:42:36 +0000

    Removed flask, wrote function stubs, setup test framework

--- phonemaster/phonemast_leases.py
+++ phonemaster/phonemast_leases.py
@@ -1,7 +1,23 @@
-from flask import Flask
+def current_rent_ascending_order():
+    """ Requirements
+    1) Produce a list sorted by “Current Rent” in ascending order
+    2) Obtain the first 5 items from the resultant list and output to the console
+    """
+    return
 
-app = Flask(__name__)
+def lease_length_25_years():
+    """ Requirements
+    1) Output the list to the console, including all data fields
+    2) Output the total rent for all items in this list to the console
+    """
+    return 
 
-# Launch app if this file was directly executed
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4000)
+def masts_count_per_tenant():
+    """ Requirements
+    1) Output the dictionary to the console in a readable form
+    """
+
+def lease_start_date_1june1999_to_31aug2007():
+    """ Requirements
+    1) Output the data to the console with dates formatted as DD/MM/YYYY
+    """
---
+++ tests/phonemast_leases_tests.py
@@ -0,0 +1,14 @@
+import os
+import unittest
+
+from context import phonemast_leases
+
+class PhonemastLeasesTest(unittest.TestCase):
+    def setup(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
