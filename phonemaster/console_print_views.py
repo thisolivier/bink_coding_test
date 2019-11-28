abe5ffa180878997ece92ec2b42dbd01ab6838bb
Olivier Butler <olivier.butler@gmail.com>
2019-11-28 00:43:47 +0000

    Finished views

--- phonemaster/console_print_views.py
+++ phonemaster/console_print_views.py
@@ -1,4 +1,5 @@
 from phonemaster.database_service import PhoneMastHelper
+from datetime import datetime
 
 large_divider = "---------------------------------"
 small_divider = "--------"
@@ -13,23 +14,42 @@ def view_rent_ascending(description, iterable_masts):
     def pretty_print_masts(someMast):
         tuple_rent_name_tenant = (PhoneMastHelper.rent(someMast), PhoneMastHelper.property_name(someMast), PhoneMastHelper.tenant_name(someMast))
         print("[%s] %s || %s" %tuple_rent_name_tenant)
+
     _print_header(description)
-    print("- Format will be: [Rent] - Location Name || Tenant")
+    print("- Format will be: [Rent] - Mast Location || Tenant")
     print(small_divider)
     [pretty_print_masts(mast) for mast in iterable_masts]
     print(large_divider)
 
-def view_lease_lengths_with_total(description, iterable_masts, total):
+def view_lease_lengths_with_total(description, iterable_masts, total_rent):
     _print_header(description)
-    
+    print("- Matching masts will be printed with the following ordered fields:")
+    print("Property Name, Property Address [1], Property  Address [2], Property Address [3], Property Address [4], Unit Name, Tenant Name, Lease Start Date, Lease End Date, Lease Years, Current Rent")
+    print(small_divider)
+    [print(mast) for mast in iterable_masts]
+    print(small_divider)
+    print("Total rent for above masts:", total_rent)
     print(large_divider)
 
 def view_mast_count_dict(description, tenant_name_count_dict):
-    _print_header(description)
+    def pretty_print_tenants(name, count):
+        print("[%s] - %s" %(count, name))
 
+    _print_header(description)
+    print("- Format will be: [Number of Masts] - Tenant")
+    print(small_divider)
+    [pretty_print_tenants(tenant_name, count) for tenant_name, count in tenant_name_count_dict.items()]
     print(large_divider)
 
 def view_masts_in_date_range(description, iterable_masts):
-    _print_header(description)
+    def pretty_print_masts(mast):
+        date = PhoneMastHelper.lease_start_date(mast)
+        formatted_date = date.strftime("%d %m %Y")
+        date_mast_tenant_tuple = (formatted_date, PhoneMastHelper.property_name(mast), PhoneMastHelper.tenant_name(mast))
+        print("[%s] %s || %s" %date_mast_tenant_tuple)
 
+    _print_header(description)
+    print("- Format will be: [date] - Mast location || Tenant")
+    print(small_divider)
+    [pretty_print_masts(mast) for mast in iterable_masts]
     print(large_divider)
